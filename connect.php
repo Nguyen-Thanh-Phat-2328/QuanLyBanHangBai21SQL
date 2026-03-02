b0b95cc1f4dd700e3000fcca1ea199869ee1fc3c
Nguyen Thanh Phat <23042004phat@gmail.com>
2026-03-02 23:40:44 +0700

    first commit

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php 
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "quanlybanhangbai21sql";
+	
+	
+	$con = new mysqli($servername, $username, $password, $database);
+
+	// Check connection
+	if ($con->connect_error) {
+	    die("Connection failed: " . $con->connect_error);
+	} 
+	// else {
+	// 	echo "Connected successfully";
+	// }
+	
+?>
